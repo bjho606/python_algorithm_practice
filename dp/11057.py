f9a502c6952ce9310475409cbcbdc8bbd6b7c849
변재호 <bjho606@naver.com>
2022-02-25 22:27:02 +0900

    dp ㅅㅜ정

--- dp/11057.py
+++ dp/11057.py
@@ -18,7 +18,7 @@ for i in range(10):
     
 for i in range(2, N+1):
     for j in range(0, 10):
-        for k in range(j, 10):
+        for k in range(j+1):
             dp[i][j] += dp[i-1][k]
 
 count = 0
