d67554c291a365fcfb24bfedc36297b182e2201a
변재호 <bjho606@naver.com>
2022-03-14 22:20:42 +0900

    floyd 토끼와 거북이 알고리즘 & tree traverse iterative(preorder만)

--- graph/1991.py
+++ graph/1991.py
@@ -1,15 +1,15 @@
 # 트리 순회
 
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+
 import sys
 input = sys.stdin.readline
 
-# 방법 1 (using class)
-# class Node:
-#     def __init__(self, val, left, right):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
+# 방법 1 (by recursive - with class)
 # def preorder(node):
 #     print(node.val, end='')
 #     if node.left != '.':
@@ -31,25 +31,41 @@ input = sys.stdin.readline
 #         postorder(tree[node.right])
 #     print(node.val, end='')
 
-# 방법 2
-def preorder(root):
-    if root != '.':
-        print(root, end='')
-        preorder(tree[root][0])
-        preorder(tree[root][1])
+# 방법 2 (by recursive - without class)
+# def preorder(root):
+#     if root != '.':
+#         print(root, end='')
+#         preorder(tree[root][0])
+#         preorder(tree[root][1])
 
-def inorder(root):
-    if root != '.':
-        inorder(tree[root][0])
-        print(root, end='')
-        inorder(tree[root][1])
+# def inorder(root):
+#     if root != '.':
+#         inorder(tree[root][0])
+#         print(root, end='')
+#         inorder(tree[root][1])
 
-def postorder(root):
-    if root != '.':
-        postorder(tree[root][0])
-        postorder(tree[root][1])
-        print(root, end='')
+# def postorder(root):
+#     if root != '.':
+#         postorder(tree[root][0])
+#         postorder(tree[root][1])
+#         print(root, end='')
 
+# 방법 3 (by iterative - with class)
+def preorder(node):
+    stack = [node]
+    while stack:
+        node = stack.pop()
+        print(node.val, end='')
+        if node.right != '.':
+            stack.append(tree[node.right])
+        if node.left != '.':
+            stack.append(tree[node.left])
+
+# def inorder(node):    // 다시 해보기
+
+# def postorder(node):  // 다시 해보기
+
+        
 N = int(input())
 tree = {}
 
@@ -58,11 +74,14 @@ for _ in range(N):
     # node, left, right = map(str, input().rstrip('\n').split())
     # tree[node] = Node(node, left, right)
     # 방법 2
-    root, left, right = map(str, input().rstrip('\n').split())
-    tree[root] = [left, right]
+    # root, left, right = map(str, input().rstrip('\n').split())
+    # tree[root] = [left, right]
+    # 방법 3
+    node, left, right = map(str, input().rstrip('\n').split())
+    tree[node] = Node(node, left, right)
 # print(tree)
 
-# 방법 1 (using class)
+# 방법 1
 # preorder(tree['A'])
 # print()
 # inorder(tree['A'])
@@ -70,10 +89,18 @@ for _ in range(N):
 # postorder(tree['A'])
 # print()
 
-# 방법 2 (using iterative)
-preorder('A')
+# 방법 2
+# preorder('A')
+# print()
+# inorder('A')
+# print()
+# postorder('A')
+# print()
+
+# 방법 3
+preorder(tree['A'])
 print()
-inorder('A')
-print()
-postorder('A')
-print()
+# inorder(tree['A'])
+# print()
+# postorder(tree['A'])
+# print()
