6cce5eb311aa18c985597a68bc0860ae498de69b
변재호 <bjho606@naver.com>
2023-02-03 20:41:43 +0900

    [greedy] 1751

---
+++ again/1751.py
@@ -0,0 +1,27 @@
+# [카드 정렬하기]
+# 힙..
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+
+    cards = []
+    for i in range(N):
+        heapq.heappush(cards, int(input()))
+    
+    min_sum = 0
+    if len(cards) == 1:
+        print(0)
+    else:
+        while len(cards) > 1:
+            cur_sum = heapq.heappop(cards) + heapq.heappop(cards)
+            min_sum += cur_sum
+            heapq.heappush(cards, cur_sum)
+
+        print(min_sum)
+
+if __name__ == "__main__":
+    solution()
